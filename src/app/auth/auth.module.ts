d7635542b74009fe289ff169ca85abf3c2983acb
Prateek Roy <royprateek96@gmail.com>
2021-06-06 18:04:45 +0530

    fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,14 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { AuthNavComponent } from './auth-nav/auth-nav.component';
 import { AuthRoutingModule } from './auth-routing.module';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent, AuthNavComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, RouterModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: [null, [Validators.required, Validators.maxLength(20)]],
+      password: [null, [Validators.required, Validators.maxLength(50)]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+  login() {}
 }
